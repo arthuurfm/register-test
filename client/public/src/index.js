70ab7557a9b6bd71fb9b2edd299db3372d73897b
Arthur Franz <arthurfranzmoura@gmail.com>
2025-06-02 12:41:58 -0300

    formatted modal

--- client/public/src/index.js
+++ client/public/src/index.js
@@ -1,20 +1,18 @@
 export default function index() {
     // Modal lógica
-    const radios = document.querySelectorAll('input[name="mode"]');
-    const sections = document.querySelectorAll('.form-section');
+    const newProductBtn = document.querySelector('.new-product-button');
+    const sections = document.querySelectorAll('[class*="form"]');
     const overlay = document.getElementById('overlay');
 
-    radios.forEach(radio => {
-      radio.addEventListener('change', () => {
-        sections.forEach(section => section.classList.remove('active'));
-        overlay.classList.add('active');
-        const form = document.querySelector(`.${radio.value}-form`);
-        form.classList.add('active');
-      });
+    newProductBtn.addEventListener('click', () => {
+      sections.forEach(section => section.classList.remove('active'));
+      overlay.classList.add('active');
+      const form = document.querySelector(`.register-form`);
+      form.classList.add('active');
     });
 
     // Fechar modal
-    const buttons = document.querySelectorAll("button");
+    const buttons = document.querySelectorAll(".cancel-button");
     buttons.forEach(btn => {
       btn.addEventListener('click', () => {
         sections.forEach(section => section.classList.remove('active'));
